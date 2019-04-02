69221400c4030f5380cc5654659c10a45b7364fe
Nick Diego Yamane <nickdiego@igalia.com>
2019-04-02 15:12:45 -0400

    Capitalize exported shell vars in env.sh
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- env.sh
+++ env.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-agldir="$(cd $(dirname $thisscript) && pwd -P)"
+AGL_DIR="$(cd $(dirname $thisscript) && pwd -P)"
 
 _has() {
     type $1 >/dev/null 2>&1
@@ -21,11 +21,11 @@ agl_bootstrap() {
     _has git || { echo "!! Error: git not installed" >&2; return 1; }
     _has python2 || { echo "!! Error: python2 not installed" >&2; return 1; }
 
-    GIT_DIR="${agldir}/.git" git submodule update --init --recursive
-    if [ ! -L "${agldir}/tools/bin/python" ]; then
-        mkdir -pv ${agldir}/tools/bin
-        ln -sv $(which python2) ${agldir}/tools/bin/python
-        ln -sv $(which python2-config) ${agldir}/tools/bin/python-config
+    GIT_DIR="${AGL_DIR}/.git" git submodule update --init --recursive
+    if [ ! -L "${AGL_DIR}/tools/bin/python" ]; then
+        mkdir -pv ${AGL_DIR}/tools/bin
+        ln -sv $(which python2) ${AGL_DIR}/tools/bin/python
+        ln -sv $(which python2-config) ${AGL_DIR}/tools/bin/python-config
     fi
 
     if [ $? -ne 0 ]; then
@@ -38,7 +38,7 @@ agl_bootstrap() {
     fi
 }
 
-py2path="${agldir}/tools/bin"
+py2path="${AGL_DIR}/tools/bin"
 if [ -d "$py2path" ]; then
     export PATH="${py2path}:${PATH}"
 else
@@ -46,25 +46,25 @@ else
 fi
 
 # Remote AGL workspace
-export buildmachine=${buildmachine:-192.168.1.110}
-export agl_ws=${agl_ws:-${agldir}/ws}
+export BUILDMACHINE=${BUILDMACHINE:-192.168.1.110}
+export AGL_WS=${AGL_WS:-${AGL_DIR}/ws}
 
 agl_mount_workspace() {
-    if findmnt -n --raw $agl_ws &>/dev/null; then
-        echo "AGL workspace already mounted at $agl_ws"
+    if findmnt -n --raw $AGL_WS &>/dev/null; then
+        echo "AGL workspace already mounted at $AGL_WS"
         return 0
     fi
-    test -d $agl_ws || mkir -p $agl_ws
+    test -d $AGL_WS || mkir -p $AGL_WS
     # TODO: Move sudo outside the function / move to separate script
-    sudo mount -t nfs "${buildmachine}:${agl_ws}" "$agl_ws"
+    sudo mount -t nfs "${BUILDMACHINE}:${AGL_WS}" "$AGL_WS"
 }
 
 
 agl_umount_workspace() {
-    if ! findmnt -n --raw $agl_ws &>/dev/null; then
+    if ! findmnt -n --raw $AGL_WS &>/dev/null; then
         echo "AGL workspace not mounted"
         return 0
     fi
     # TODO: Move sudo outside the function / move to separate script
-    sudo umount "$agl_ws"
+    sudo umount "$AGL_WS"
 }
